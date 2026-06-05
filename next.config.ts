5d568f9ffb2bf0f53939b1b9cfdd0aacb9e00869
zoooesfG <44790812+zoooesf@users.noreply.github.com>
2026-06-05 11:17:24 -0400

    pdf fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
-import path from "path";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
